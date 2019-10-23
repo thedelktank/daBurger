f51a623aa55278e9f42f6f64beba1f8e5b3fce60
Trevor Delker <tddelker@gmail.com>
2019-10-22 18:34:44 -0600

    heroku config

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var mysql = require('mysql');
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection=mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: 'mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'mkpgktohpitxqxdy',
     password: 'bubgv1kdo51yc96f',
     database: 'ig5f30phsi1rdef5'
 })
-
+}
+;
 connection.connect(function(err){
     if(err)throw err;
-    console.log('connected as id: '+connection.threadid);
+    console.log('connected as id: '+ connection.threadid);
 });
 
 module.exports = connection;
